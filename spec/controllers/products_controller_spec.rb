685b056b2571bc9dfeb4735ec2d7f17b09e389fd
imaishiuta <shuta25le0n@gmail.com>
2019-01-11 17:12:45 +0900

    Edit products_controller_spec.rb

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -46,6 +46,21 @@ describe ProductsController do
     end
   end
 
+  describe 'GET #search' do
+
+    it "#searchのビューの表示" do
+      get :search,params: {keyword: "テ"}
+      expect(response).to render_template :search
+    end
+
+    it "検索フォームから送信されたキーワードで検索できるか" do
+      create(:product,category_id: 340)
+      get :search, params: {keyword: "テ" }
+      expect(assigns(:products).length).to eq 1
+    end
+  end
+
+
   describe 'DELETE #destroy' do
     it "商品消去" do
       product = create(:product,category_id: 340)
@@ -53,15 +68,12 @@ describe ProductsController do
         delete :destroy, params: { id: product}
       end.to change(Product, :count).by(-1)
     end
-
     it "商品消去と同時に紐づく商品画像も消去できる" do
       product = create(:product,category_id: 340)
       image = create(:product_image,product_id: product.id)
       expect do
         delete :destroy, params: { id: product}
       end.to change(ProductImage, :count).by(-1)
-
     end
   end
-
 end
