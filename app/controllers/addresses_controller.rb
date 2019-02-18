768acdf0fc06ce149df3922e794977c3c8c1963b
imaishiuta <shuta25le0n@gmail.com>
2019-02-18 14:13:47 +0900

    edit address_controller.rb
    change the page to redirect by the path

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,16 +1,33 @@
 class AddressesController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create]
+
   def new
      @address = Address.new
   end
 
   def create
-     @address = Address.new(address_params)
-     if @address.save
-       redirect_to register_cregit_card_path
-     else
-       redirect_to new_address_path
-     end
+    @address = Address.new(address_params)
+    path = Rails.application.routes.recognize_path(request.referrer)
+    if @address.save
+      if path[:controller] == "mypayjp"
+        redirect_to product_mypayjp_path(path)
+      elsif path[:controller] == "addresses"
+        redirect_to register_cregit_card_path
+      end
+    else
+      redirect_to new_address_path
+    end
+  end
+
+  def edit
+    @address = Address.find_by(user_id: current_user.id)
+  end
+
+  def update
+    @address = Address.find_by(user_id: current_user.id)
+    @address.update(address_params)
+    path = Rails.application.routes.recognize_path(request.referrer)
+    redirect_to product_mypayjp_path(path)
   end
 
   private
